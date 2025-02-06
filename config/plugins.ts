f614bb6dec091e2615ad94c3f7c8552cc22e8597
Michiel <Michiel@mctol.nl>
2025-02-06 22:35:01 +0100

    goy it running on DO db

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+export default () => {
+  
+    return {
+      upload: {
+        config: {
+          provider: "@strapi/provider-upload-cloudinary",
+          providerOptions: {
+            cloud_name: process.env.CLOUDINARY_NAME,
+            api_key: process.env.CLOUDINARY_KEY,
+            api_secret: process.env.CLOUDINARY_SECRET,
+          },
+          actionOptions: {
+            upload: {},
+            delete: {},
+          },
+        },
+      },
+    };
+  };
+  
